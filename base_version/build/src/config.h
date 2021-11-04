74a55e7979efa848209b0d87788b175649e0a575
Hemmy Gerrits <h.gerrits1@student.han.nl>
2021-11-04 12:44:41 +0100

    newrobotworld

---
+++ base_version/build/src/config.h
@@ -0,0 +1,119 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* "" */
+#define BOOST_TIMER_LIBS ""
+
+/* Defined if the requested minimum BOOST version is satisfied */
+#define HAVE_BOOST 1
+
+/* Define to 1 if you have <boost/archive/text_oarchive.hpp> */
+#define HAVE_BOOST_ARCHIVE_TEXT_OARCHIVE_HPP 1
+
+/* Defined if the Boost chrono library is available */
+#define HAVE_BOOST_CHRONO 1
+
+/* Define to 1 if you have <boost/chrono.hpp> */
+#define HAVE_BOOST_CHRONO_HPP 1
+
+/* Defined if the Boost date_time library is available */
+#define HAVE_BOOST_DATE_TIME 1
+
+/* Define to 1 if you have <boost/date_time/posix_time/posix_time.hpp> */
+#define HAVE_BOOST_DATE_TIME_POSIX_TIME_POSIX_TIME_HPP 1
+
+/* Defined if the Boost filesystem library is available */
+#define HAVE_BOOST_FILESYSTEM 1
+
+/* Define to 1 if you have <boost/filesystem/path.hpp> */
+#define HAVE_BOOST_FILESYSTEM_PATH_HPP 1
+
+/* Defined if the Boost regex library is available */
+#define HAVE_BOOST_REGEX 1
+
+/* Define to 1 if you have <boost/regex.hpp> */
+#define HAVE_BOOST_REGEX_HPP 1
+
+/* Defined if the Boost serialization library is available */
+#define HAVE_BOOST_SERIALIZATION 1
+
+/* Defined if the Boost system library is available */
+#define HAVE_BOOST_SYSTEM 1
+
+/* Define to 1 if you have <boost/system/error_code.hpp> */
+#define HAVE_BOOST_SYSTEM_ERROR_CODE_HPP 1
+
+/* Define to 1 if you have <boost/test/unit_test.hpp> */
+#define HAVE_BOOST_TEST_UNIT_TEST_HPP 1
+
+/* Defined if the Boost thread library is available */
+#define HAVE_BOOST_THREAD 1
+
+/* Define to 1 if you have <boost/thread.hpp> */
+#define HAVE_BOOST_THREAD_HPP 1
+
+/* Defined if the Boost unit_test_framework library is available */
+#define HAVE_BOOST_UNIT_TEST_FRAMEWORK 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* define if the wxWidgets 3.0.2. or higher is available */
+#define HAVE_WXWIDGETS 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "robotworld-0.2.0"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "J.Kraaijeveld@askesis.nl"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "robotworld"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "robotworld 0.2.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "robotworld-0.2.0"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.2.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.2.0"
